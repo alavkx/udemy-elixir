22c4176f58f6af10492a68d725465b02e04f9140
Alec Lavoie <alavoie3@gmail.com>
2019-06-25 16:07:17 -0400

    feat: use contexts to describe discuss app

---
+++ update/lib/update/discussions/comment.ex
@@ -0,0 +1,18 @@
+defmodule Update.Discussions.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "comments" do
+    field :content, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content])
+    |> validate_required([:content])
+  end
+end
