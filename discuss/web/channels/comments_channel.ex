64c768f9755abf80e1944d73cfe5212ddfc539d1
Alec Lavoie <alavoie3@gmail.com>
2019-06-25 15:40:09 -0400

    feat(comments): broadcast websocket updates, add user id

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,18 +1,36 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
-  alias Discuss.Topic
+  alias Discuss.{Topic, Comment}
 
   def join("comments:" <> topic_id, _payload, socket) do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id)
 
-    {:ok, %{}, socket}
+    topic =
+      Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(comments: [:user])
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, message, socket) do
-    IO.puts(name)
-    IO.inspect(message)
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+    user_id = socket.assigns[:user_id]
 
-    {:reply, :ok, socket}
+    changeset =
+      topic
+      |> build_assoc(:comments, user_id: user_id)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        comment = Repo.preload(comment, :user)
+
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
+        {:reply, :ok, socket}
+
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -3,16 +3,42 @@ import { Socket } from "phoenix"
 let socket = new Socket("/socket", { params: { token: window.userToken } })
 socket.connect()
 
-
 function createSocket(topicId) {
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => renderComments(resp.comments))
     .receive("error", resp => { console.log("Unable to join", resp) })
 
+  channel.on(`comments:${topicId}:new`, renderComment)
+
   document.querySelector('button').addEventListener('click', function () {
-    channel.push('comment:hello', { hi: 'there!' })
+    const content = document.querySelector('textarea').value
+    channel.push('comment:add', { content })
   })
+}
+
+function commentTemplate(comment) {
+  const emailEle = document.createElement('div')
+  emailEle.className = "secondary-content"
+  emailEle.innerText = comment.user ? comment.user.email : 'Anon'
+  const contentEle = document.createElement('li')
+  contentEle.className = "collection-item"
+  contentEle.innerText = comment.content
+  contentEle.appendChild(emailEle)
+  return contentEle;
+}
+
+function renderComment(event) {
+  const commentElement = commentTemplate(event.comment);
+  const commentContainerElement = document.querySelector('.collection')
+  commentContainerElement.appendChild(commentElement)
+}
+
+function renderComments(comments) {
+  const commentElements = comments.map(commentTemplate)
+  const commentContainerElement = document.querySelector('.collection')
+  commentContainerElement.innerHTML = '';
+  commentElements.forEach(x => commentContainerElement.appendChild(x))
 }
 
 window.createSocket = createSocket
